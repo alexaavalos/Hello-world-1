3c825e68a61a4e5b36773cdc54f3b7561a4ae6cf
alexaavalos <113448535+alexaavalos@users.noreply.github.com>
2022-11-22 15:37:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#this is my first python program
+"""
+this code is written in python 3.8
+interpreted by the console
+"""
+
+print ("Hello World!")
+print (50)
+print (3.10)
+print (50,"letters",3.10)
+print ("hello", end = " ")
+print ("world")
